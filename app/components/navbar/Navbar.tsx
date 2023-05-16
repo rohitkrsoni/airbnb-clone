f1120981b317e06e321ab1b7230f91ce0d78e8f6
rohitkrsoni <krrohit37@gmail.com>
2023-05-17 00:43:27 +0530

    Add SearchBar Usermenu Avatar in Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
@@ -17,6 +19,8 @@ const Navbar = () => {
             "
           >
             <Logo />
+            <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
