88f6fad491f28273cc4e3034625f73de097d03ba
rohitkrsoni <krrohit37@gmail.com>
2023-05-18 14:10:13 +0530

    Register UI completed Added Google Github in footer

--- app/components/modals/RegisterModel.tsx
+++ app/components/modals/RegisterModel.tsx
@@ -11,6 +11,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModel = () => {
   const registerModal = useRegisterModel();
@@ -75,6 +76,47 @@ const RegisterModel = () => {
     </div>
   );
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+
+      <div
+        className="
+          text-neutral-500
+          text-center
+          mt-4
+          font-light
+      "
+      >
+        <div className="flex flex-row gap-2 items-center justify-center">
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className="
+              text-neutral-800
+              cursor-pointer
+              hover:underline
+            "
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       disabled={isLoading}
@@ -84,6 +126,7 @@ const RegisterModel = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
